048cbb8502da4de5dcb8457074eea30cb7870474
Arimsingle <Arim.coe60@gmail.com>
2020-04-25 19:09:32 +0700

    Re

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
 // pages/_app.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import React from 'react'
+import App, { Container } from 'next/app'
+import withRedux from 'next-redux-wrapper';
+import { store } from '../src/redux/store';
+import { Provider } from 'react-redux';
+
+class MyApp extends App {
+    render() {
+        const { Component, pageProps, store } = this.props;
+
+        return (
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
+        )
+    }
 }
+// export default function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+export default withRedux(store)(MyApp);
--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,6 @@ export default class MyDocument extends Document {
                 <title>CAR 3D</title>
                 <Head >
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/antd/4.0.2/antd.min.css' />
                     {this.props.styleTags}
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
                 </Head>
